99b7b49968746899fe370b8f5616e639dd789909
CYT1107 <86656163+CYT1107@users.noreply.github.com>
2021-07-01 23:27:27 +0800

    finished_2

--- reviews.py
+++ reviews.py
@@ -6,5 +6,30 @@ with open ('reviews.txt', 'r') as f:
         count = count + 1
         if count % 1000 == 0:
             print(len(data))
-print(len(data)) #印出留言筆數
-print(data[0]) #印出第一筆
+print('檔案讀取完了，總共有', len(data), '筆資料')
+
+#加總留言長度
+sum_len = 0
+for word in data:
+    sum_len = sum_len + len(word)
+
+#平均留言長度
+print('平均是', sum_len/len(data))
+
+#篩選小於100字的留言
+new = []
+for d in data:
+    if len(d) < 100:
+        new.append(d)
+print('一共有', len(new), '筆留言長度小於100')
+
+#印出長度小於100字的留言中的第一筆
+print(new[0])
+
+#篩選所有有good的留言
+good = []
+for d in data:
+    if 'good' in d:
+        good.append(d)
+print('一共有', len(good), '筆留言提到good')
+print(good[0])
